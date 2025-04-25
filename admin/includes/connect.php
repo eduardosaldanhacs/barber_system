27412abb821abf5546d4637c106f90a7b5f2121b
Eduardo Saldanha <102400077+eduardosaldanhacs@users.noreply.github.com>
2025-04-25 09:41:55 -0300

    Update connect.php

--- admin/includes/connect.php
+++ admin/includes/connect.php
@@ -4,9 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//$conn = new mysqli('localhost','vovoolmi_ra','vovo@@olmira2021','vovoolmi_ra');
-$conn = new mysqli('localhost','root','','testeadm');
-//$conn = new mysqli('localhost','webdbs73_vovo','6qSPU7-r-h)w','webdbs73_vovo');
+
+$conn = new mysqli('','','','');
+
 
 
 ?>
