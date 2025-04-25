0aa8d6f5f91a2052178e3b30b7106c8df7d1781d
Eduardo Saldanha <102400077+eduardosaldanhacs@users.noreply.github.com>
2025-04-25 09:43:02 -0300

    Update connect.php

--- admin/includes/connect.php
+++ admin/includes/connect.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 $conn = new mysqli('localhost','root','','testeadm');
 
-//$conn = new mysqli('ftp.eduardosaldanha.online','edu81877_admin', '[4UGxn+J,GjN', 'edu81877_barbearia_estilo');
+//$conn = new mysqli('','', '', '');
 
 ?>
 
